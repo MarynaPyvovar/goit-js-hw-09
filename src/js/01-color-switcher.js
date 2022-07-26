22c5e9150152bfac88d72e73897437d59e86dbe0
MarynaPyvovar <marina18renkas@gmail.com>
2022-07-26 16:06:20 +0300

    feat 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let id = null;
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  if (startBtn.hasAttribute('disabled')) {
+    return;
+  }
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'true');
+  id = setInterval(changeBodyColor, 1000);
+}
+
+function onStopClick() {
+  if (stopBtn.hasAttribute('disabled')) {
+    return;
+  }
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+  clearInterval(id);
+}
+
+function changeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
